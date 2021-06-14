c00a17a5bec50d3e630131efa0e15fd1bea95bff
HanaBarazandeh <haniebarazande@gmail.com>
2021-06-14 16:10:43 -0700

    using fragments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import khiaalImg from './khiaal.png';
+
+function Header(props) {
+  console.log(props);
+  return (
+      <h1>{props.name}'s Website</h1>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <img src="https://github.com/hanabarazandeh.png" height={300} alt="my github picture"/>
+      <ul>
+        {props.dishes.map((dish)=>(
+        <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <section>
+      <p>{props.year}</p>
+    </section>
+  );
+}
+
+const dishes=[
+  "pasta",
+  "butter chicken",
+  "joojeh kabab"
+];
+
+const dishObjects = dishes.map((dish, i) => ({id: i, title:dish}));
+console.log(dishObjects);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Hana" />
+      <Main dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
