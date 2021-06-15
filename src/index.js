640c4cd1991c7353a0e6323bb8399a4790bbe633
HanaBarazandeh <haniebarazande@gmail.com>
2021-06-14 21:37:49 -0700

    destructuring arrays and objects for cleaner syntax, like {authorized} instead of props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const checklist= ["boots", "tent", "headlamp"];
+console.log(checklist[1]);
 // ReactDOM.render(
 //   React.createElement("h1",
 //   {style:{color:"blue"}},
