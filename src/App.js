0fe45eae8b9f192fabdde7540b397c609d207663
HanaBarazandeh <haniebarazande@gmail.com>
2021-06-15 19:27:44 -0700

    checkbox input

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import React, { useState, useEffect } from "react";
-
 import './App.css';
 
 function App() {
-  // const what = useState("happy");
-  // console.log(what);
-
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(function () {
-    console.log(`It's ${emotion} around here`);
-  }, [emotion]);
-
-  useEffect(function () {
-    console.log(`It's ${secondary} around here`);
-  }, [secondary]);
-
-
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}</h1>
-      <button onClick={
-        function () {
-          return setEmotion("sad");
-        }
-      }>sad
-      </button>
-      <button onClick={
-        function () {
-          return setEmotion("enthusiastic");
-        }
-      }>enthuse
-      </button>
-      <button onClick={
-        function () {
-          return setSecondary("crabby");
-        }
-      }>crabby
-      </button>
-      <button onClick={
-        function () {
-          return setSecondary("tired");
-        }
-      }>tired
-      </button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={
+          function () {
+            setChecked(
+              (checked) => !checked
+            )
+          }
+        }ƒ
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
